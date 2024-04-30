5a0c444c3c21d1e2837e404865840620622c44b8
uditaa-garg <95528879+uditaa-garg@users.noreply.github.com>
2024-04-30 23:06:34 +0530

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 22:23:31 2024
+
+@author: KIIT
+"""
+
+import streamlit as st
+import joblib
+
+def main():
+    html_temp = """
+    <div style="background-color:black;color:white;padding:16px">
+    health insurance  </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    
+    
